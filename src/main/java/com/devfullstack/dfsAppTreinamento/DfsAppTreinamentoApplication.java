22545bcdd0f8937dc2154d6c9f758ce4eb504ce9
Leandro Amorim <leandroamorimpb@gmail.com>
2025-08-24 10:38:01 -0300

    Criando o projeto, e configurando os perfis properties de test, dev, prod

---
+++ src/main/java/com/devfullstack/dfsAppTreinamento/DfsAppTreinamentoApplication.java
@@ -0,0 +1,13 @@
+package com.devfullstack.dfsAppTreinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsAppTreinamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfsAppTreinamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devfullstack/dfsAppTreinamento/DfsAppTreinamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfullstack.dfsAppTreinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfsAppTreinamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
